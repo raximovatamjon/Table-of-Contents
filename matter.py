421643b900e9370f9db2372e3bcd1247b1ced3e8
Atamjon <139760000+raximovatamjon@users.noreply.github.com>
2024-11-23 00:44:43 +0500

    matter
    i solved matters from our teacher gave in the lessons whose name is Shoxrux Malikov

---
+++ matter.py
@@ -0,0 +1,219 @@
+
+#1-masala
+
+# for i in range(20):
+#     if i == 4 or i == 9 or i == 14 or i ==19:
+#         print("\n")
+#         continue
+#     print("*", end=" ")
+    
+    
+#2-masala
+# for i in range(16): 
+#     if i == 0 or i == 4 or i == 8 or i == 12 or i == 16:
+#         if i  == 4 or i == 8 or i == 12 or i == 16:            
+#             print("\n")
+#             print("#", end=" ")
+#             continue
+#         print("#", end=" ")
+#         continue
+
+#     print("*", end=" ")
+
+#3-masala
+# for i in range(20):
+#     if i >= 0 and i <= 3 :
+#         print("#",end=" ")
+#         if i == 3:
+#             print("\n")
+#         continue
+#     if i == 4 or i == 8 or i == 12 or i == 16:
+#         if i  == 4 or i == 8 or i == 12 or i == 16:            
+#             print("\n")
+#             print("#", end=" ")
+#             continue
+#         print("#", end=" ")
+#         continue
+
+#     print("*", end=" ")
+
+#4-masala
+# for i in range(17):
+#     if i >= 0 and i <= 3:
+#         print("#",end=" ") 
+#         continue 
+#     if i == 4:
+#         print("\n")
+#         continue
+    
+#     if i == 5 or i == 9 or i == 13 or i == 17:
+#         if i  == 5 or i == 9 or i == 13 or i == 17:            
+#             print("\n")
+#             print("#", end=" ")
+#             continue
+#         print("#", end=" ")
+#         continue
+#     if i  == 8 or i == 12 or i == 16 :            
+#             # print("\n")
+#             print("#", end=" ")
+#             continue
+
+#     print("*", end=" ")
+    
+#5masala
+
+# for i in range (16): #qator
+#     for j in range(16):#ustun
+#         if i == j:
+#             if i == 5 or i == 6 or i == 9 or i == 10 :
+#                 print("*", end=" ")
+#             else:
+#                 print("#",end=" ")
+#             if i == 3 or i == 7 or i == 11 or i == 15:
+#                 print("\n")
+
+
+#6masala
+# for i in range (20): #qator
+#     for j in range(20):#ustun
+#         if i == j:
+#             if i == 5 or i == 6 or i == 13 or i == 14 :
+#                 print("*", end=" ")
+#             else:
+#                 print("#",end=" ")
+#             if i == 3 or i == 7 or i == 11 or i == 15:
+#                 print("\n")
+
+#7masala
+# for i in range (20): #qator
+#     for j in range(20):#ustun
+#         if i == j:
+#             if i == 5 or i == 6 or i == 13 or i == 14 :
+#                 print("0", end=" ")
+#             else:
+#                 print("#",end=" ")
+#             if i == 3 or i == 7 or i == 11 or i == 15:
+#                 print("\n")
+
+#8masala
+# for i in range (25): #qator
+#     for j in range(25):#ustun
+#         if i == j:
+#             if i == 0 or i == 6 or i == 12 or i == 18 or i == 24 :
+#                 print("0", end=" ")
+#             else:
+#                 print("#",end=" ")
+#             if i == 4 or i == 9 or i == 14 or i == 19 or i == 24:
+#                 print("\n")
+
+#9masala
+# for i in range (25): #qator
+#     for j in range(25):#ustun
+#         if i == j:
+#             if i == 0 or i == 5 or i == 6 or i == 10 or i == 11 or i == 12 or i == 15 or i == 16 or i == 17 or i ==18 or i >19 :
+#                 print("0", end=" ")
+#             else:
+#                 print("#",end=" ")
+#             if i == 4 or i == 9 or i == 14 or i == 19 or i == 24:
+#                 print("\n")
+
+#10-masala
+# for i in range (25): #qator
+#     for j in range(25):#ustun
+#         if i == j:
+#             if i % 2 == 0:
+#                 print("0", end=" ")
+            
+#             else:
+#                 print("1", end=" ")
+#             if i == 4 or i == 9 or i == 14 or i == 19 or i == 24:
+#                     print("\n")
+
+
+#11-masala
+# for i in range (25): #qator
+#     for j in range(25):#ustun
+#         if i == j:
+#             if i % 2 == 0 :
+#                 if i == 0 or i == 4 or i == 6 or i == 8 or i == 12 or i == 16 or i==18 or i ==20 or i ==24:
+#                     print("0", end=" ")
+#                 else:
+#                     print("1", end=" ")
+            
+#             else:
+#                 print("1", end=" ")
+#             if i == 4 or i == 9 or i == 14 or i == 19 or i == 24:
+#                     print("\n")
+
+#12-masala
+# for i in range (25): #qator
+#     for j in range(25):#ustun
+#         if i == j:
+#             if i % 2 == 0:
+#                 print("0", end=" ")
+            
+#             else:
+#                 print("1", end=" ")
+#             if i == 4 or i == 9 or i == 14 or i == 19 or i == 24:
+#                     print("\n")
+
+
+#11-masala
+# for i in range (25): #qator
+#     for j in range(25):#ustun
+#         if i == j:
+            
+
+#             if i == 7 or i == 11 or i == 13 or i == 17:
+#                  print("1", end=" ")            
+#             else:
+#                 print("0", end=" ")
+#             if i == 4 or i == 9 or i == 14 or i == 19 or i == 24:
+#                 print("\n")
+            
+
+
+#12-masala
+
+# for i in range (25): #qator
+#     for j in range(25):#ustun
+#         if i == j:            
+
+#             if i >= 0 and i <= 4:
+#                  print("A", end=" ") 
+#             elif i >= 5 and i <= 9:
+#                  print("B", end=" ")  
+#             elif i >= 10 and i <= 14:
+#                  print("C", end=" ") 
+#             elif i >= 15 and i <= 19:
+#                  print("D", end=" ") 
+#             elif i >= 20 and i <= 24:
+#                  print("E", end=" ")           
+            
+#             if i == 4 or i == 9 or i == 14 or i == 19 or i == 24:
+#                 print("\n")
+
+#13-masala
+# for i in range (25): #qator
+#     for j in range(25):#ustun
+#         if i == j:            
+
+#             if i == 0 :
+#                  print("1", end=" ") 
+#             elif i == 2 :
+#                  print("2", end=" ")  
+#             elif i == 3 :
+#                  print("3", end=" ")  
+#             elif i == 4 :
+#                  print("4", end=" ")  
+#             elif i == 5 :
+#                  print("5", end=" ")  
+#             elif i == 6 :
+#                  print("6", end=" ")  
+#             elif i == 7 :
+#                  print("7", end=" ")  
+#             elif i == 8 :
+#                  print("8", end=" ")                 
+            
+#             if i == 4 or i == 9 or i == 14 or i == 19 or i == 24:
+#                 print("\n")
